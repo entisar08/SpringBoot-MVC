feb4e78563d79ad61ca723cab63529531fa18de6
entisar08 <eelmohbat@gmail.com>
2022-09-25 13:00:50 -0500

    MVC concept intro

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/spring08mvccontroller/controller/LogInController.java
@@ -0,0 +1,14 @@
+package com.cydeo.spring08mvccontroller.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LogInController {
+
+    @RequestMapping("/login")
+    public String login(){
+
+        return "login.html";
+    }
+}
