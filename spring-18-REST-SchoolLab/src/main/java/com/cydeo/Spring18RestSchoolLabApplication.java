35f31ba95016e230a5de08a0241e0a2e1a8454de
entisar08 <eelmohbat@gmail.com>
2022-12-04 21:14:50 -0600

    API school lab school lab

---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/Spring18RestSchoolLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring18RestSchoolLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring18RestSchoolLabApplication.class, args);
+    }
+
+}
