907c1d3777f5a7ba0bc65f61637485ec54afef4b
entisar08 <eelmohbat@gmail.com>
2022-12-16 21:32:03 -0600

    test case added

--- spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
+++ spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
 
-    public static String operator = "add";
+    public static String operator = "add";//created this fireld to use assert true by compating to strings
 
     public static int add(int num1, int num2) {
         return num1 + num2;
--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,7 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
+    @DisplayName("Adding Method")
     void add() {
+
+        int actual = Calculator.add(2,3);
+        assertEquals(5, actual, "Test faild ");
+
+    }
+    @Test
+
+    void cas2() {
+
+        assertEquals("add",Calculator.operator);
+        assertTrue(Calculator.operator.equals("add"));
 
     }
 }
