36f949d55b9859be85c08cb03caeaf49a1bdc455
entisar08 <eelmohbat@gmail.com>
2022-12-17 00:43:23 -0600

    life cycle hook annotation

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -5,28 +5,92 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    @BeforeAll
+    static void setUpAll() {
+        System.out.println("BeforeAll is executed.");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        System.out.println("AfterAll is executed.");
+    }
+
+    @BeforeEach
+    void setUpEach() {
+        System.out.println("BeforeEach is executed.");
+    }
+
+    @AfterEach
+    void tearDownEach() {
+        System.out.println("AfterEach is executed.");
+    }
+
     @Test
-    @DisplayName("Adding Method")
+    @DisplayName("MyMethod")
     void add() {
-
-        int actual = Calculator.add(2,3);
-        assertEquals(5, actual, "Test faild ");
-
+        System.out.println("Add method");
+        int actual = Calculator.add(2, 3);
+        assertEquals(5, actual, "Test failed.");
     }
+
     @Test
+    void add2() {
+        System.out.println("Add2 method");
 
-    void cas2() {
+//        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(3, 2));
+//        assertThrows(AccessDeniedException.class, () -> Calculator.add2(3, 2));
+        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(2, 3));
+    }
 
-        assertEquals("add",Calculator.operator);
+    @Test
+    void testCase1() {
+//        System.out.println("Test Case 1");
+        fail("Not implemented yet.");
+    }
+
+    @Test
+    void testCase2() {
+        System.out.println("Test Case 2");
+        assertEquals("add", Calculator.operator);
         assertTrue(Calculator.operator.equals("add"));
-
-    }
-@Test
-    void cas3() {
-
-        assertArrayEquals(new int[]{1,2,3}, new int[] {1,3,2});
-
     }
 
+    @Test
+    void testCase3() {
+        System.out.println("Test Case 3");
+        assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 3, 2}, "Arrays are not same.");
+    }
+
+    @Test
+    void testCase4() {
+
+        System.out.println("Test Case 4");
+
+        String nullString = null;
+        String notNullString = "Cydeo";
+
+        assertNull(nullString);
+        assertNotNull(notNullString);
+
+//        assertNull(notNullString);
+        assertNotNull(nullString);
+
+    }
+
+    @Test
+    void testCase5() {
+
+        System.out.println("Test Case 5");
+
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+        assertSame(c1, c2);
+        assertNotSame(c1, c3);
+
+    }
+
+    // maven-surefire-plugin
 
 }
