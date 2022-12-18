4feaae5eaf9fbc153316bf26a1f69be5f20657eb
entisar08 <eelmohbat@gmail.com>
2022-12-18 13:07:19 -0600

    Parameterized  Test

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
@@ -51,11 +51,11 @@ public class CalculatorParameterizedTest {
     }
 
 
-    /*@ParameterizedTest
+    @ParameterizedTest
     @CsvFileSource(resources = "/sample-data.csv", numLinesToSkip = 1)
     void testCase6(int num1, int num2, int result) {
         Assertions.assertEquals(result, Calculator.add(num1, num2));
-    }*/
+    }
 
 
 }
