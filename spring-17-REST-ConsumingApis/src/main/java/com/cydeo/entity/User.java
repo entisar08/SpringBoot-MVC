91116c26bb63028f1f432b8b3599d41f99c99c75
entisar08 <eelmohbat@gmail.com>
2022-12-21 00:00:05 -0600

    Cienma lab User Repo update

---
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,25 @@
+package com.cydeo.entity;
+
+import com.cydeo.entity.enums.UserRole;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "user_account")
+public class User extends BaseEntity {
+
+    private String email;
+
+    private String password;
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_details_id")
+    private Account account;
+
+}
