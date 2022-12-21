7d2d4e1262ed3067b9fee31d81d91820c17da8d6
entisar08 <eelmohbat@gmail.com>
2022-12-20 23:59:09 -0600

    Cienma lab Movie service

---
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/entity/enums/MovieState.java
@@ -0,0 +1,5 @@
+package com.cydeo.entity.enums;
+
+public enum MovieState {
+    ACTIVE,DRAFT,SUSPENDED;
+}
---
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/entity/enums/MovieType.java
@@ -0,0 +1,5 @@
+package com.cydeo.entity.enums;
+
+public enum MovieType {
+    PREMIER,REGULAR;
+}
